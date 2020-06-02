6307b5bf72a7da06eb85582be952f321a3316cd3
Aylton Almeida <almeida@aylton.dev>
2020-06-02 12:24:57 -0300

    database[+]

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,22 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (req: express.Request, res: express.Response) => {
-  console.log("Listagem de usuários");
+app.use(express.json());
 
-  res.json(["Diego", "Cleiton", "Robson"]);
-});
+app.use(routes);
+
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+// GEt: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informaçào existente no back-end
+// DELETE: Remover uma informação do back-end
+
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parâmetros para criação/atualizaçào de informaçòes
 
 app.listen(3333);
+console.log("Server running on port 3333");
